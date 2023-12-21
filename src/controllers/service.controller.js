a73b272d29bec1d0af210e301f7ca8fb8c96676c
ratel4150 <rratel76@gmail.com>
2023-12-21 17:42:38 -0600

    🛠️🚀feat(controller): agregar función para crear un nuevo servicio
    Desarrolla la función 'createService' en el controlador de servicios para permitir la creación de un nuevo servicio. Utiliza el esquema de validación Joi 'createServiceSchema' para validar los datos del cuerpo de la solicitud antes de realizar la inserción en la base de datos. La función 'insertServiceToDatabase' ejecuta una consulta SQL para insertar el nuevo servicio y devuelve el ID generado. Se manejan errores y se envían respuestas JSON claras según el resultado.
    
    👉 Detalles:
    - Nueva función 'createService' para crear un servicio con datos validados.
    - Utiliza el esquema de validación Joi 'createServiceSchema'.
    - Ejecuta una consulta SQL para insertar el nuevo servicio en la base de datos.
    - Respuestas JSON enviadas al cliente según el resultado de la operación.
    - Mejora la seguridad y robustez de la creación de servicios.
    
    Maneja errores y proporciona respuestas claras en caso de éxito o fallo al intentar crear un nuevo servicio. Esta adición mejora la API al garantizar la validación adecuada y la creación segura de servicios.

--- src/controllers/service.controller.js
+++ src/controllers/service.controller.js
@@ -19,3 +19,103 @@ export const getPlaceServiceByUserId = async (req, res) => {
       return res.status(404).json({message: 'service not found'})
     }  
   }
+
+
+  /**
+ * Joi schema for creating a service.
+ * @typedef {Object} CreateServiceSchema
+ * @property {string} name - The name of the service. Required.
+ * @property {string} image - The image URL of the service.
+ * @property {boolean} active - Indicates whether the service is active. Required.
+ * @property {number} order - The order of the service (integer).
+ * @property {string} mobile_app_icon - The icon URL for the mobile app.
+ */
+/**
+ * Define the validation schema for creating a service.
+ * @type {Joi.ObjectSchema<CreateServiceSchema>}
+ */
+const createServiceSchema = Joi.object({
+  nombre: Joi.string(),
+  imagen: Joi.string(),
+  activo: Joi.boolean(),
+  orden: Joi.number().integer(),
+  icono_app_movil: Joi.string(),
+});
+
+
+/**
+ * Creates a new service using the provided data.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Promise<void>}
+ * @throws {Error} Throws an error if the service creation fails.
+ */
+export const createService = async (req, res) => {
+  try {
+    // Validate the request body data with the schema
+    const { error, value } = createServiceSchema.validate(req.body);
+
+    if (error) {
+      // If there are validation errors, respond with a 400 error and the error details
+      return res
+        .status(400)
+        .json({ message: "Invalid request body", error: error.details });
+    }
+
+    const serviceData = extractServiceData(value);
+
+    await insertServiceToDatabase(serviceData);
+
+    // Send a success response or additional data as needed
+    res.json({ message: "Service created successfully" });
+  } catch (error) {
+    // Log the error and send a 500 status with a JSON response
+    console.error(error);
+    res.status(500).json({ message: "Failed to create service" });
+  }
+};
+
+
+
+const insertServiceToDatabase = async (serviceData) => {
+  const place_id = 0;
+  const sequelize = getDatabaseInstance(place_id);
+
+  // Execute stored procedure or perform actions to create a new service
+  const [serviceCreated, metadata] = await sequelize.query(
+    `
+    INSERT INTO [dbo].[servicio] (nombre, imagen, activo, orden, icono_app_movil)
+  VALUES (:nombre, :imagen, :activo, :orden, :icono_app_movil);
+  SELECT SCOPE_IDENTITY() AS newServiceId;
+  `,
+    {
+      replacements: serviceData,
+    }
+  );
+
+  console.log(serviceCreated);
+
+  // Check if the service was created successfully
+  if (!(serviceCreated && serviceCreated.length > 0)) {
+    // If the service creation was not successful, throw an error
+    throw new Error("Failed to create service");
+  }
+};
+
+
+const extractServiceData = (requestBody) => {
+  const { nombre, imagen, activo, orden, icono_app_movil } = requestBody;
+
+ /*  if (!nombre || !imagen || !activo || !orden || !icono_app_movil) {
+    throw new Error("Invalid request body. Missing required properties.");
+  } */
+
+  return {
+    nombre,
+    imagen,
+    activo,
+    orden,
+    icono_app_movil,
+  };
+};
