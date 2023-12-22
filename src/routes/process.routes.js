47ca61f788f8966aded64cd5a627094fffd76e58
ratel4150 <rratel76@gmail.com>
2023-12-22 10:11:20 -0600

    ✨🔧feat(route): agregar ruta y lógica para crear categoría de proceso
    Esta actualización agrega la ruta 'POST /processes' en el archivo de rutas, permitiendo la creación de una nueva categoría de proceso. La ruta está vinculada al controlador 'createProcess' del archivo 'process.controller.js'. La lógica en el controlador utiliza un esquema de validación Joi ('createProcessSchema') para verificar la integridad de los datos proporcionados antes de realizar la inserción en la base de datos. Además, se aprovecha la función 'insertProcessToDatabase' para ejecutar la inserción SQL y 'extractProcessData' para mejorar la modularidad del código.
    
    👉 Detalles:
    - Nueva ruta 'POST /processes' para la creación de categorías de proceso.
    - Controlador 'createProcess' maneja la lógica de creación con validación de datos.
    - Esquema de validación Joi ('createProcessSchema') garantiza datos válidos.
    - Utilización de funciones 'insertProcessToDatabase' y 'extractProcessData'.
    
    Este commit mejora la estructura de la API, proporcionando una forma segura y validada de crear nuevas categorías de proceso.

--- src/routes/process.routes.js
+++ src/routes/process.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express'
-import {getPlaceServiceProcessByUserId} from '../controllers/process.controller.js'
+import * as processController from '../controllers/process.controller.js';
 import { authRequired } from '../middlewares/validateToken.js'
 import {validateSchema} from '../middlewares/validator.middleware.js'
 import {registerSchema, loginSchema} from '../schemas/auth.schema.js'
 
 const router = Router()
 
-router.get('/PlaceServiceProcessByUserId/:user_id', getPlaceServiceProcessByUserId)
+router.get('/PlaceServiceProcessByUserId/:user_id', processController.getPlaceServiceProcessByUserId)
+// Ruta para crear un nuevo proceso
+router.post('/processes', processController.createProcess);
 
 export default router
