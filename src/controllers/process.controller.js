959541a1694c095abbecc0ac3fad492302efbb7a
ratel4150 <rratel76@gmail.com>
2023-12-22 10:07:11 -0600

    ✨🔄feat(controller): agregar lógica y ruta para crear una nueva categoría de proceso
    Implementa la creación de una nueva categoría de proceso en el controlador 'createProcess' y su correspondiente ruta 'POST /processes'. La lógica utiliza un esquema de validación Joi ('createProcessSchema') para verificar la validez de los datos de la solicitud antes de realizar la inserción en la base de datos. Se utiliza el método 'INSERT INTO' de SQL para agregar la nueva categoría de proceso en la tabla 'proceso'. Además, se proporciona una función de extracción de datos ('extractProcessData') para mejorar la modularidad.
    
    👉 Detalles:
    - Nueva ruta 'POST /processes' para la creación de una categoría de proceso.
    - Controlador 'createProcess' para manejar la lógica de creación de categorías de proceso.
    - Esquema de validación Joi ('createProcessSchema') para garantizar la validez de los datos.
    - Uso de la función 'insertProcessToDatabase' para realizar la inserción en la base de datos.
    - Función 'extractProcessData' para extraer y validar los datos del cuerpo de la solicitud.
    
    Este commit mejora la API al agregar funcionalidad para crear nuevas categorías de procesos de manera segura y validada.

--- src/controllers/process.controller.js
+++ src/controllers/process.controller.js
@@ -1,5 +1,5 @@
 import { getDatabaseInstance } from "../config/dbManager.config.js";
-
+import Joi from "joi";
 export const getPlaceServiceProcessByUserId = async (req, res) => {
   
   const place_id = 0 
@@ -19,3 +19,137 @@ export const getPlaceServiceProcessByUserId = async (req, res) => {
       return res.status(404).json({message: 'process not found'})
     }  
   }
+
+
+  /**
+ * Validation schema for creating a process.
+ * @typedef {Object} CreateProcessSchema
+ * @property {string} name - The name of the process. Required.
+ * @property {string} image - The image URL of the process.
+ * @property {boolean} active - Indicates whether the process is active. Required.
+ * @property {string} stored_procedure_management - The stored procedure for management.
+ * @property {string} stored_procedure_management_graphic - The stored procedure for graphical management.
+ * @property {string} management_table- The management table.
+ * @property {string} mobile_application_url - The mobile application URL.
+ */
+const createProcessSchema = Joi.object({
+  nombre: Joi.string(),
+  imagen: Joi.string(),
+  activo: Joi.boolean(),
+  procedimiento_almacenado_gestion: Joi.string(),
+  procedimiento_almacenado_gestion_grafico: Joi.string(),
+  tabla_gestion: Joi.string(),
+  url_aplicacion_movil: Joi.string(),
+  });
+
+  /**
+ * Creates a new process category using the provided data.
+ *
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Promise<void>}
+ * @throws {Error} Throws an error if the process category creation fails.
+ */
+export const createProcess = async (req, res) => {
+  try {
+    // Validate the request body data with the schema
+    const { error, value } = createProcessSchema.validate(req.body);
+
+    if (error) {
+      // If there are validation errors, respond with a 400 error and the error details
+      return res
+        .status(400)
+        .json({ message: "Invalid request body", error: error.details });
+    }
+
+    const processData = extractProcessData(value);
+
+    await insertProcessToDatabase(processData);
+
+    // Send a success response or additional data as needed
+    res.json({ message: "Process category created successfully" });
+  } catch (error) {
+    // Log the error and send a 500 status with a JSON response
+    console.error(error);
+    res.status(500).json({ message: "Failed to create process category" });
+  }
+};
+
+/**
+ * Inserts process data into the database.
+ *
+ * @param {Object} processData - The data to be inserted into the database.
+ * @returns {Promise<void>} - A Promise that resolves once the data is inserted.
+ * @throws {Error} - Throws an error if the insertion fails.
+ */
+export const insertProcessToDatabase = async (processData) => {
+  const place_id = 0;
+  const sequelize = getDatabaseInstance(place_id);
+
+  // Execute stored procedure or perform actions to create a new process category
+  const [processCreated, metadata] = await sequelize.query(
+    `
+    INSERT INTO dbo.proceso (
+      nombre,
+      imagen,
+      activo,
+      procedimiento_almacenado_gestion,
+      procedimiento_almacenado_gestion_grafico,
+      tabla_gestion,
+      url_aplicacion_movil
+    )
+    VALUES (
+      :nombre,
+      :imagen,
+      :activo,
+      :procedimiento_almacenado_gestion,
+      :procedimiento_almacenado_gestion_grafico,
+      :tabla_gestion,
+      :url_aplicacion_movil
+    );
+  `,
+    {
+      replacements: processData,
+    }
+  );
+
+  /* // Check if the process was created successfully
+  if (!(processCreated && processCreated.length > 0)) {
+    // If the process creation was not successful, throw an error
+    throw new Error("Failed to create process category");
+  } */
+};
+
+// Helper function to extract process data
+/**
+ * Extracts process data from the request body.
+ *
+ * @param {Object} requestBody - The request body containing process data.
+ * @returns {Object} - The extracted process data.
+ * @throws {Error} - Throws an error if the extraction fails or if required properties are missing.
+ */
+export const extractProcessData = (requestBody) => {
+  const {
+    nombre,
+    imagen,
+    activo,
+    procedimiento_almacenado_gestion,
+    procedimiento_almacenado_gestion_grafico,
+    tabla_gestion,
+    url_aplicacion_movil,
+  } = requestBody;
+
+ /*  if (!nombre || !imagen  || !procedimiento_almacenado_gestion || !procedimiento_almacenado_gestion_grafico || !tabla_gestion || !url_aplicacion_movil) {
+    throw new Error("Invalid request body. Missing required properties.");
+  } */
+
+  return {
+    nombre,
+    imagen,
+    activo,
+    procedimiento_almacenado_gestion,
+    procedimiento_almacenado_gestion_grafico,
+    tabla_gestion,
+    url_aplicacion_movil,
+  };
+};
