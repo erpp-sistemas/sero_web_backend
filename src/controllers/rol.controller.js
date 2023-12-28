ebe20399cc866c5fe0963b7dece494f8d1059421
ratel4150 <rratel76@gmail.com>
2023-12-28 17:42:44 -0600

    "Agregar función: Obtener todos los roles"
    Implementada la funcionalidad para recuperar todos los roles almacenados en la base de datos. Se ha creado una nueva ruta GET /api/roles para manejar esta operación. La consulta SQL utilizada selecciona los campos id_rol, nombre y activo de la tabla de roles. Se ha manejado cualquier posible error y se envía una respuesta JSON con la lista de roles recuperados.
    
    Características incluidas:
    - Ruta: GET /api/roles
    - Consulta SQL para recuperar roles
    - Manejo de errores y respuestas JSON claras

--- src/controllers/rol.controller.js
+++ src/controllers/rol.controller.js
@@ -95,4 +95,30 @@ export const createRol = async (req, res) => {
       activo,
     };
   };
+  /**
+ * Obtiene todos los roles de la base de datos.
+ *
+ * @param {Object} req - Objeto de solicitud Express.
+ * @param {Object} res - Objeto de respuesta Express.
+ * @returns {Promise<void>}
+ * @throws {Error} Lanza un error si falla la recuperación.
+ */
+export const getAllRoles = async (req, res) => {
+    try {
+      const place_id = 0;
+      const sequelize = getDatabaseInstance(place_id);
+  
+      // Ejecuta la consulta para obtener todos los roles
+      const [roles, metadata] = await sequelize.query(`
+        SELECT id_rol, nombre, activo FROM db_prueba.dbo.rol;
+      `);
+  
+      // Envía los roles recuperados como una respuesta JSON
+      res.json(roles);
+    } catch (error) {
+      // Registra el error y envía un estado 500 con una respuesta JSON
+      console.error(error);
+      res.status(500).json({ message: "Error al recuperar los roles" });
+    }
+  };
   
