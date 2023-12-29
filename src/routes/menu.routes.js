2b76986ca3286e2e412d34069bfe5aa5be4ec713
ratel4150 <rratel76@gmail.com>
2023-12-29 14:23:48 -0600

    "Añadir rutas de API para operaciones CRUD en Menús
    Se agregan rutas para obtener menús por ID de usuario, crear, obtener todos, actualizar y eliminar menús. Las rutas están documentadas con JSDoc en español."

--- src/routes/menu.routes.js
+++ src/routes/menu.routes.js
@@ -3,9 +3,23 @@ import {getMenusUserId} from '../controllers/menu.controller.js'
 import { authRequired } from '../middlewares/validateToken.js'
 import {validateSchema} from '../middlewares/validator.middleware.js'
 import {registerSchema, loginSchema} from '../schemas/auth.schema.js'
+import * as menuController from '../controllers/menu.controller.js';
 
 const router = Router()
 
-router.get('/MenusUserId/:user_id', getMenusUserId)
+/**
+ * Rutas para crud menu.
+ * @route DELETE /menus/{id}
+ * @group Menús - Operaciones relacionadas con Menús
+ * @returns {object} 200 - Menú eliminado exitosamente.
+ * @returns {Error} 404 - Menú no encontrado.
+ * @returns {Error} 500 - Error interno del servidor.
+ */
+
+router.get('/MenusUserId/:user_id', menuController.getMenusUserId)
+router.post('/menus', menuController.createMenu);
+router.get('/menus', menuController.getAllMenus);
+router.put('/menus/{id}', menuController.updateMenu);
+router.delete('/menus/{id}', menuController.deleteMenu);
 
 export default router
