331591ce1858ff2eb576818b51ba3b82dd318a55
ratel4150 <rratel76@gmail.com>
2024-01-03 09:44:08 -0600

    Agregar rutas CRUD para submenú se agrgean rutas para el submenu

---
+++ src/routes/submenu.routes.js
@@ -0,0 +1,22 @@
+import {Router} from 'express'
+import {getPlaceServiceByUserId} from '../controllers/service.controller.js'
+import { authRequired } from '../middlewares/validateToken.js'
+import {validateSchema} from '../middlewares/validator.middleware.js'
+import {registerSchema, loginSchema} from '../schemas/auth.schema.js'
+import * as submenuController from '../controllers/submenus.controller.js'
+const router = Router()
+
+// Create a new sub-menu
+router.post('/submenus',submenuController.createSubMenu);
+
+// Get all sub-menus
+router.get('/submenus', submenuController.getAllSubMenus);
+
+// Update a specific sub-menu by ID
+router.put('/submenus/:id', submenuController.updateSubMenu);
+
+// Delete a specific sub-menu by ID
+router.delete('/submenus/:id', submenuController.deleteSubMenu);
+
+
+export default router
