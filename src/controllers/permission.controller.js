8a9612caa0c4c4fcfc78bb9bb3cbaf51fee90731
ratel4150 <rratel76@gmail.com>
2024-01-05 10:59:03 -0600

    Implementar endpoint de creación de menu_rol
    Descripción del Commit:
    Se implementó un nuevo endpoint llamado createMenuRol para gestionar la creación de entradas menu_rol en la base de datos. El endpoint valida el cuerpo de la solicitud según el esquema especificado, extrae los datos necesarios e los inserta en la base de datos mediante una consulta Sequelize. Además, se introdujo una función auxiliar llamada extractMenuRolData para garantizar que las propiedades requeridas estén presentes en el cuerpo de la solicitud. Se implementó manejo de errores para responder de manera adecuada en caso de fallos en la validación o inserción en la base de datos.

--- src/controllers/permission.controller.js
+++ src/controllers/permission.controller.js
@@ -10,6 +10,14 @@ const createSubMenuRolSchema = Joi.object({
   activo: Joi.boolean().required(),
 });
 
+// Define el esquema de validación para los datos del cuerpo de la solicitud de menu_rol
+const createMenuRolSchema = Joi.object({
+    id_menu_rol: Joi.number(),
+    id_menu: Joi.number(),
+    id_rol: Joi.number(),
+    activo: Joi.boolean().required(),
+  });
+
 /**
  * Inserts sub_menu_rol data into the database.
  *
@@ -226,6 +234,100 @@ const extractSubMenuRolData = (requestBody) => {
     }
   
     return { id_sub_menu, id_rol, activo };
+  };
+
+
+
+
+  
+/**
+ * Inserts menu_rol data into the database.
+ *
+ * @param {Object} menuRolData - The data to be inserted into the database.
+ * @returns {Promise<void>} - A Promise that resolves once the data is inserted.
+ * @throws {Error} - Throws an error if the insertion fails.
+ */
+const insertMenuRolToDatabase = async (menuRolData) => {
+    const place_id = 0;
+    const sequelize = getDatabaseInstance(place_id);
+  
+    // Execute stored procedure or perform actions to create a new menu_rol entry
+    const [menuRolCreated] = await sequelize.query(
+      `
+      INSERT INTO db_prueba.dbo.menu_rol (id_menu, id_rol, activo)
+      VALUES (:id_menu, :id_rol, :activo);
+      SELECT SCOPE_IDENTITY() AS insertedId;
+    `,
+      {
+        replacements: menuRolData,
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
+  
+    console.info(menuRolCreated);
+  
+    // Check if the menu_rol entry was created successfully
+    if (
+      !menuRolCreated ||
+      menuRolCreated.length === 0 ||
+      !menuRolCreated.insertedId
+    ) {
+      // If the menu_rol creation was not successful, throw an error
+      throw new Error("Failed to create menu_rol entry");
+    }
+    return menuRolCreated.insertedId;
+  };
+  
+  /**
+   * Creates a new menu_rol entry using the provided data.
+   *
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @returns {Promise<void>}
+   * @throws {Error} Throws an error if the menu_rol entry creation fails.
+   */
+  export const createMenuRol = async (req, res) => {
+    try {
+      // Validar los datos del cuerpo de la solicitud con el esquema
+      const { error, value } = createMenuRolSchema.validate(req.body);
+  
+      if (error) {
+        // Si hay errores de validación, responde con un error 400 y los detalles del error
+        return res
+          .status(400)
+          .json({ message: "Invalid request body", error: error.details });
+      }
+  
+      const menuRolData = extractMenuRolData(value);
+  
+      await insertMenuRolToDatabase(menuRolData);
+  
+      // Send a success response or additional data as needed
+      res.json({ message: "menu_rol entry created successfully" });
+    } catch (error) {
+      // Log the error and send a 500 status with a JSON response
+      console.error(error);
+      res.status(500).json({ message: "Failed to create menu_rol entry" });
+    }
+  };
+
+
+
+  /**
+ * Extracts menu_rol data from the request body.
+ *
+ * @param {Object} requestBody - The request body containing menu_rol data.
+ * @returns {Object} - The extracted menu_rol data.
+ * @throws {Error} - Throws an error if the extraction fails or if required properties are missing.
+ */
+const extractMenuRolData = (requestBody) => {
+    const { id_menu, id_rol, activo } = requestBody;
+  
+    if (!id_menu || !id_rol || !activo) {
+      throw new Error("Invalid request body. Missing required properties.");
+    }
+  
+    return { id_menu, id_rol, activo };
   };
 
 
