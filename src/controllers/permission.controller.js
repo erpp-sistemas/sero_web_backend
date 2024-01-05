f25185d8b4524442dd670406019acaec3c55bb83
ratel4150 <rratel76@gmail.com>
2024-01-05 11:16:29 -0600

    Implementar funciones para la creación de entradas sub_menu_rol_usuario
    Descripción del Commit:
    Se agregaron funciones para la creación de entradas de sub_menu_rol_usuario en la base de datos. La función insertSubMenuRolUsuarioToDatabase ejecuta una consulta para insertar nuevos datos de sub_menu_rol_usuario en la base de datos, mientras que createSubMenuRolUsuario utiliza estas funciones para crear una nueva entrada en respuesta a una solicitud HTTP. Además, se proporciona una función extractSubMenuRolUsuarioData para extraer y validar los datos del cuerpo de la solicitud antes de realizar la inserción. Se implementó manejo de errores para responder adecuadamente en caso de fallos en la validación o inserción de datos.

--- src/controllers/permission.controller.js
+++ src/controllers/permission.controller.js
@@ -27,6 +27,16 @@ const createMenuRolUsuarioSchema = Joi.object({
     activo: Joi.boolean().required(),
   });
 
+
+  // Define el esquema de validación para los datos del cuerpo de la solicitud de sub_menu_rol_usuario
+const createSubMenuRolUsuarioSchema = Joi.object({
+    id_sub_menu_rol_usuario: Joi.number(),
+    id_sub_menu: Joi.number(),
+    id_rol: Joi.number(),
+    id_usuario: Joi.number(),
+    activo: Joi.boolean().required(),
+  });
+
 /**
  * Inserts sub_menu_rol data into the database.
  *
@@ -679,6 +689,105 @@ export const deleteMenuRolUsuario = async (req, res) => {
     }
   
     return { id_menu, id_rol, id_usuario, activo };
+  };
+
+
+
+
+
+
+
+  
+/**
+ * Inserts sub_menu_rol_usuario data into the database.
+ *
+ * @param {Object} subMenuRolUsuarioData - The data to be inserted into the database.
+ * @returns {Promise<void>} - A Promise that resolves once the data is inserted.
+ * @throws {Error} - Throws an error if the insertion fails.
+ */
+const insertSubMenuRolUsuarioToDatabase = async (subMenuRolUsuarioData) => {
+    const place_id = 0;
+    const sequelize = getDatabaseInstance(place_id);
+  
+    // Execute stored procedure or perform actions to create a new sub_menu_rol_usuario entry
+    const [subMenuRolUsuarioCreated] = await sequelize.query(
+      `
+      INSERT INTO db_prueba.dbo.sub_menu_rol_usuario (id_sub_menu, id_rol, id_usuario, activo)
+      VALUES (:id_sub_menu, :id_rol, :id_usuario, :activo);
+      SELECT SCOPE_IDENTITY() AS insertedId;
+    `,
+      {
+        replacements: subMenuRolUsuarioData,
+        type: sequelize.QueryTypes.SELECT,
+      }
+    );
+  
+    console.info(subMenuRolUsuarioCreated);
+  
+    // Check if the sub_menu_rol_usuario entry was created successfully
+    if (
+      !subMenuRolUsuarioCreated ||
+      subMenuRolUsuarioCreated.length === 0 ||
+      !subMenuRolUsuarioCreated.insertedId
+    ) {
+      // If the sub_menu_rol_usuario creation was not successful, throw an error
+      throw new Error("Failed to create sub_menu_rol_usuario entry");
+    }
+    return subMenuRolUsuarioCreated.insertedId;
+  };
+  
+  /**
+   * Creates a new sub_menu_rol_usuario entry using the provided data.
+   *
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @returns {Promise<void>}
+   * @throws {Error} Throws an error if the sub_menu_rol_usuario entry creation fails.
+   */
+  export const createSubMenuRolUsuario = async (req, res) => {
+    try {
+      // Validar los datos del cuerpo de la solicitud con el esquema
+      const { error, value } = createSubMenuRolUsuarioSchema.validate(req.body);
+  
+      if (error) {
+        // Si hay errores de validación, responde con un error 400 y los detalles del error
+        return res
+          .status(400)
+          .json({ message: "Invalid request body", error: error.details });
+      }
+  
+      const subMenuRolUsuarioData = extractSubMenuRolUsuarioData(value);
+  
+      await insertSubMenuRolUsuarioToDatabase(subMenuRolUsuarioData);
+  
+      // Send a success response or additional data as needed
+      res.json({ message: "sub_menu_rol_usuario entry created successfully" });
+    } catch (error) {
+      // Log the error and send a 500 status with a JSON response
+      console.error(error);
+      res.status(500).json({ message: "Failed to create sub_menu_rol_usuario entry" });
+    }
+  };
+
+
+
+
+  
+/**
+ * Extracts sub_menu_rol_usuario data from the request body.
+ *
+ * @param {Object} requestBody - The request body containing sub_menu_rol_usuario data.
+ * @returns {Object} - The extracted sub_menu_rol_usuario data.
+ * @throws {Error} - Throws an error if the extraction fails or if required properties are missing.
+ */
+const extractSubMenuRolUsuarioData = (requestBody) => {
+    const { id_sub_menu, id_rol, id_usuario, activo } = requestBody;
+  
+    if (!id_sub_menu || !id_rol || !id_usuario || !activo) {
+      throw new Error("Invalid request body. Missing required properties.");
+    }
+  
+    return { id_sub_menu, id_rol, id_usuario, activo };
   };
 
 
